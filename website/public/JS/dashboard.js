9287cbbd1c1501fb1cdefaf133809b16ece5824a
Zlang HD <126128932+zlanghd@users.noreply.github.com>
2024-01-21 16:36:00 +0700

    Create dashboard.js

---
+++ website/public/JS/dashboard.js
@@ -0,0 +1,13 @@
+VANTA.BIRDS({
+  el: "#body",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  backgroundColor: 0x0,
+  color1: 0x0000ff,
+  color2: 0xffff00,
+});
